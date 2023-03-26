dacc1ee6135ef95281a940c082015321f6754ad3
adamlui@protonmail.com <adamlui@protonmail.com>
2023-03-26 15:25:42 -0700

    Synced from adamlui/userscripts

---
+++ src/greasemonkey/README.md
@@ -0,0 +1,85 @@
+<h1>
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/bravegpt/media/images/bravegpt-logo-dark-mode-730x135.png">
+    <img alt="BraveGPT" width=322 height-58 src="https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/bravegpt/media/images/bravegpt-logo-light-mode-730x135.png">
+  </picture>
+  <a href="https://www.producthunt.com/posts/bravegpt?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-bravegpt" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=385630&theme=light" alt="BraveGPT - Bring&#0032;the&#0032;magic&#0032;of&#0032;ChatGPT&#0032;to&#0032;Brave&#0032;Search&#0033; | Product Hunt" style="width: 156px; height: 34px; margin:0 0 11px 5px;" width="156" height="34" /></a>
+  <a href="https://twitter.com/intent/tweet?text=This%20add-on%20adds%20%23ChatGPT%20answers%20right%20besides%20Brave%20Search%20results%21&url=https://www.bravegpt.com&hashtags=greasemonkey,javascript,ai"><img alt="Tweet" style="margin:0 0 14px 5px;" src="https://img.shields.io/twitter/url/http/shields.io.svg?style=social"></a>
+  
+</h1>
+
+[![license](https://img.shields.io/badge/License-MIT-green.svg)](LICENSE.md)
+[![support](https://img.shields.io/badge/Support-Chrome/Firefox/Edge/Opera/Brave/Vivaldi/LibreWolf/QQ-989898.svg)](#compatibility)
+
+<h3><b>BraveGPT</b> brings the magic of ChatGPT to search results.</h3>
+
+<a href="https://greasyfork.org/en/scripts/462440-bravegpt"><img alt="Install this script" src="https://raw.githubusercontent.com/adamlui/userscripts/master/install-button.svg"></a><a href="#installation"><img alt="How to install" title="How to install" src="https://github.com/adamlui/userscripts/raw/master/help-button.svg"></a>
+<br>
+[Discuss](https://github.com/adamlui/userscripts/discussions) / 
+[Review](https://greasyfork.org/en/scripts/462440-bravegpt/feedback#post-discussion) / 
+[Contact author](https://elonsucks.org/@adam)
+
+#
+
+<img src="https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/bravegpt/media/images/screenshots/brave-ai.png">
+
+## Installation
+
+1. Install Tampermonkey ([Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo), [Firefox](https://addons.mozilla.org/firefox/addon/tampermonkey/), [Edge](https://microsoftedge.microsoft.com/addons/detail/tampermonkey/iikmkjmpaadaobahmlepeloendndfphd), [Opera](https://addons.opera.com/en/extensions/details/tampermonkey-beta/)) or Violentmonkey ([Chrome](https://chrome.google.com/webstore/detail/violent-monkey/jinjaccalgkegednnccohejagnlnfdag), [Firefox](https://addons.mozilla.org/firefox/addon/violentmonkey/), [Edge](https://microsoftedge.microsoft.com/addons/detail/violentmonkey/eeagobfjdenkkddmbclomhiblgggliao))
+
+2. Install [BraveGPT](https://greasyfork.org/en/scripts/462440-bravegpt) (will load in userscript manager installed above)
+
+3. Visit [search.brave.com](https://search.brave.com), type your search, engage bot!
+
+<div align="center">
+
+![bravegpt-demo](https://user-images.githubusercontent.com/10906554/227480635-2ab5270e-d3f1-4857-8d6c-c4abd5eb6956.gif)
+
+</div>
+
+## Compatibility
+
+BraveGPT is compatible with Chrome, Firefox, Edge, Opera, Brave, Vivaldi, LibreWolf & QQ using Tampermonkey (recommended due to streaming suppport resulting in faster responses) or Violentmonkey. ([Pull requests](https://github.com/adamlui/userscripts/pulls) always welcome!)
+
+<a href="https://greasyfork.org/en/scripts/462440-bravegpt"><img alt="Install this script" src="https://raw.githubusercontent.com/adamlui/userscripts/master/install-button.svg"></a><a href="#installation"><img alt="How to install" title="How to install" src="https://github.com/adamlui/userscripts/raw/master/help-button.svg"></a>
+<br>
+[Discuss](https://github.com/adamlui/userscripts/discussions) /
+[Leave a review](https://greasyfork.org/en/scripts/462440-bravegpt/feedback#post-discussion) /
+[More ChatGPT userscripts](https://github.com/adamlui/userscripts/tree/master/chatgpt)
+
+## Social links
+
+<a href="https://technodon.org/@bravegpt" target="_blank"><img align="bottom" src="https://img.shields.io/mastodon/follow/110077227154250890?domain=https%3A%2F%2Ftechnodon.org&style=social" alt="Follow @bravegpt on Mastodon" title="Mastodon">
+[![Follow @adamlui on GitHub](https://img.shields.io/github/followers/adamlui?label=Follow%20%40adamlui&style=social "GitHub")](https://github.com/adamlui)
+[![Follow @AdamLuiTube on YouTube](https://img.shields.io/youtube/channel/subscribers/UCgBMqK7SRL5R__3qM-YAcSg?label=Follow%20%40AdamLuiTube&style=social)](https://www.youtube.com/AdamLuiTube?sub_confirmation=1)
+<a href="https://elonsucks.org/@adam" target="_blank"><img align="bottom" src="https://img.shields.io/mastodon/follow/109387703022229926?domain=https%3A%2F%2Felonsucks.org&style=social" alt="Follow @adam on Mastodon" title="Mastodon">
+[![Follow @adamllui on Twitter](https://img.shields.io/twitter/follow/adamllui?style=social)](https://twitter.com/adamllui)
+<br><br>
+
+## Related scripts
+
+### <picture><source media="(prefers-color-scheme: dark)" srcset="https://i.imgur.com/RduASbD.png"><img width=16 src="https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon64.png"></picture> [ChatGPT Auto Refresh ↻](https://github.com/adamlui/userscripts/tree/master/chatgpt/chatgpt-auto-refresh)
+
+Keeps your ChatGPT sessions active to avoid constant Cloudflare checks.
+<br>[Install](https://greasyfork.org/en/scripts/462422-chatgpt-auto-refresh) / 
+[Readme](https://github.com/adamlui/userscripts/tree/master/chatgpt/chatgpt-auto-refresh/README.md) / 
+[Discuss](https://github.com/adamlui/userscripts/discussions)
+
+### <img src="https://raw.githubusercontent.com/adamlui/duckduckgpt/main/media/images/ddgpt-icon48.png" width=17> [DuckDuckGPT](https://github.com/adamlui/userscripts/tree/master/chatgpt/duckduckgpt) <a href="https://www.producthunt.com/posts/duckduckgpt?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-duckduckgpt" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=379261&theme=light" alt="DuckDuckGPT - Bring&#0032;the&#0032;magic&#0032;of&#0032;ChatGPT&#0032;to&#0032;DuckDuckGo | Product Hunt" style="width: 112px; height: 24px; margin:0 0 -4px 5px;" width="112" height="24" /></a>
+
+Display ChatGPT answers in DuckDuckGo sidebar.
+<br>[Install](https://greasyfork.org/en/scripts/459849-duckduckgpt) / 
+[Readme](https://github.com/adamlui/userscripts/tree/master/chatgpt/duckduckgpt/README.md) / 
+[Discuss](https://github.com/adamlui/userscripts/discussions)
+
+### <picture><source media="(prefers-color-scheme: dark)" srcset="https://i.imgur.com/RduASbD.png"><img width=16 src="https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon64.png"></picture> [ChatGPT Widescreen Mode 🖥️](https://github.com/adamlui/userscripts/tree/master/chatgpt/chatgpt-widescreen-mode)
+
+Adds Widescreen + Full-Window modes to ChatGPT for reduced scrolling.
+<br>[Install](https://greasyfork.org/en/scripts/461473-chatgpt-widescreen-mode) / 
+[Readme](https://github.com/adamlui/userscripts/tree/master/chatgpt/chatgpt-widescreen-mode/README.md) / 
+[Discuss](https://github.com/adamlui/userscripts/discussions)
+
+#
+
+<a href="https://github.com/adamlui/userscripts">**More userscripts**</a> / 
+<a href="#----------------">Back to top ↑</a>
---
+++ src/greasemonkey/bravegpt.user.js
@@ -0,0 +1,140 @@
+// ==UserScript==
+// @name             BraveGPT 🤖
+// @version          2023.03.25.3
+// @author           Adam Lui
+// @namespace        https://github.com/adamlui
+// @description      Adds ChatGPT answers to Brave Search sidebar
+// @homepageURL      https://www.bravegpt.com
+// @supportURL       https://github.com/adamlui/userscripts/issues
+// @license          MIT
+// @icon             https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/bravegpt/media/images/bravegpt-icon48.png
+// @icon64           https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/bravegpt/media/images/bravegpt-icon64.png
+// @compatible       chrome
+// @compatible       firefox
+// @compatible       edge
+// @compatible       opera
+// @compatible       brave
+// @compatible       vivaldi
+// @compatible       librewolf
+// @compatible       qq
+// @match            https://search.brave.com/*
+// @connect          gpt.bot.nu
+// @connect          api.pawan.krd
+// @grant            GM.xmlHttpRequest
+// @downloadURL      https://greasyfork.org/scripts/462440/code/bravegpt.user.js
+// @updateURL        https://greasyfork.org/scripts/462440/code/bravegpt.meta.js
+// ==/UserScript==
+
+(function() {
+    var endpointMap = { 'https://gpt.bot.nu' : '',
+                        'https://api.pawan.krd' : 'pk-pJNAtlAqCHbUDTrDudubjSKeUVgbOMvkRQWMLtscqsdiKmhI' }
+    var braveGPTdivAlerts = { tooManyRequests: 'ChatGPT is flooded with too many requests. Check back later!' }
+
+    // Stylize ChatGPT container + children
+    var braveGPTstyle = document.createElement('style')
+    braveGPTstyle.innerText = `
+        .chatgpt-container { word-wrap: break-word ; white-space: pre-wrap ; margin-bottom: 20px }
+        .chatgpt-container p { margin: 0 }
+        .chatgpt-container .chatgpt-icon { position: relative ; bottom: -4px ; margin-right: 11px }
+        .chatgpt-container .prefix { font-size: 20px ; font-family: var(--brand-font) }
+        .chatgpt-container .loading { color: #b6b8ba ; animation: pulse 2s cubic-bezier(.4,0,.6,1) infinite }
+        .chatgpt-container pre { /* ChatGPT output box */
+            /* text spacing */ white-space: pre-wrap ; line-height: 21px ;
+            font-family: Consolas, Menlo, Monaco, monospace ;
+            /* box spacing */ padding: 0.55em ; margin: .5em 0 ; border-radius: 5px ;
+            background-color: #eaeaea
+        }
+        .chatgpt-container .footer {
+            margin: 20px 0 -32px 0 ; padding-top: 17px !important ; font-size: var(--text-sm-2)
+            justify-content: right !important
+        }
+        .chatgpt-container .footer a { margin-right: -22px /* to counter .snippet padding */ }
+        .chatgpt-container .feedback { font-family: var(--brand-font) ; color: var(--search-text-06);
+            font-size: .65rem ; letter-spacing: .02em ; line-height: 1; position: relative ; right: -222px }
+        .chatgpt-container .feedback .icon { fill: currentColor ; color: currentColor ; --size:15px }
+        .chatgpt-container .footer a:hover { color: black }
+        @keyframes pulse { 0%, to { opacity: 1 } 50% { opacity: .5 }}`
+    document.head.appendChild(braveGPTstyle) // append style to <head>
+
+    // Create BraveGPT container & add id/classes
+    var braveGPTdiv = document.createElement('div') // create container div
+    braveGPTdiv.setAttribute('id', 'infobox')
+    braveGPTdiv.setAttribute( // assign Brave's .snippet + custom class
+        'class', 'snippet chatgpt-container')
+
+    // Create feedback footer & add class/HTML
+    var chatGPTfooter = document.createElement('div') // create footer div
+    chatGPTfooter.setAttribute('class', 'footer')
+    chatGPTfooter.innerHTML = `<a class="feedback svelte-8js1iq" target="_blank" href="https://github.com/adamlui/userscripts/discussions/new/choose"><svg class="icon" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15"><path fill-rule="evenodd" d="M.577 6.23a.577.577 0 1 1 0-1.153H1.5a.577.577 0 0 1 0 1.154H.577ZM2.83 8.939a.576.576 0 0 1 0 .816l-1.385 1.385a.573.573 0 0 1-.816 0 .576.576 0 0 1 0-.816l1.385-1.385a.577.577 0 0 1 .816 0ZM.63.985a.576.576 0 1 1 .815-.816L2.83 1.553a.576.576 0 1 1-.816.816L.63.985ZM15 5.654a.577.577 0 0 1-.577.577H13.5a.577.577 0 0 1 0-1.154h.923c.319 0 .577.258.577.577Zm-.631 4.669a.576.576 0 1 1-.816.816l-1.385-1.385a.576.576 0 1 1 .816-.816l1.385 1.385Zm-2.2-7.954a.576.576 0 0 1 0-.816L13.553.17a.577.577 0 0 1 .816.816l-1.385 1.384a.575.575 0 0 1-.816 0ZM9.3 9.09a.579.579 0 0 0-.045.038c-.45.417-.486 1.23-.486 1.47v.238c-1.045.45-2.053.177-2.537-.013v-.226c0-.24-.036-1.053-.487-1.469a.687.687 0 0 0-.044-.037c-.81-.609-1.777-1.667-1.777-3.253 0-2.073 1.604-3.76 3.576-3.76s3.577 1.687 3.577 3.76c0 1.586-.967 2.644-1.777 3.252Zm-1.8 4.757c-.995 0-1.223-.623-1.27-.814v-.997a4.83 4.83 0 0 0 1.343.197c.374 0 .78-.057 1.195-.18v.978c-.05.202-.282.816-1.269.816ZM7.5.923c-2.609 0-4.73 2.204-4.73 4.914 0 1.616.757 3.047 2.192 4.141.058.094.114.39.115.618v2.494c0 .03.003.06.007.09.1.63.732 1.82 2.416 1.82s2.316-1.19 2.416-1.82a.674.674 0 0 0 .006-.09v-2.494c0-.206.054-.525.11-.613 1.438-1.096 2.198-2.528 2.198-4.146 0-2.71-2.121-4.914-4.73-4.914Z" clip-rule="evenodd"></path></svg> Feedback</a>`
+
+    main() // run main routine
+
+    // Functions
+
+    function getUserscriptManager() {
+        try { return GM_info.scriptHandler } catch (error) { return 'other' }}
+
+    function braveGPTdivAlert(msg) {
+        braveGPTdiv.innerHTML = `<p>${ braveGPTdivAlerts[msg] }</p>` }
+
+    function braveGPTConsoleError(msg) { console.error('🦁 BraveGPT >> ' + msg) }
+    function braveGPTConsoleWarn(msg) { console.warn('🦁 BraveGPT >> ' + msg) }
+
+    async function getShowAnswer(question, callback) {
+        var userscriptManager = getUserscriptManager()
+        if (!getShowAnswer.triedAPIhosts) getShowAnswer.triedAPIhosts = []
+        if (!getShowAnswer.attemptCnt) getShowAnswer.attemptCnt = 0
+
+        // Randomize API proxy
+        var apiHosts = Object.keys(endpointMap).filter(function(apiHost) {
+            return !getShowAnswer.triedAPIhosts?.includes(apiHost) })
+        var randomAPIhost = apiHosts[Math.floor(Math.random() * apiHosts.length)]
+        var accessKey = endpointMap[randomAPIhost]
+        var randomEndpoint = randomAPIhost + '/v1/chat/completions'
+
+        GM.xmlHttpRequest({
+            method: 'POST', url: randomEndpoint,
+            headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${ accessKey }` },
+            responseType: 'text',
+            data: JSON.stringify({
+                messages: [{ role: 'user', content: question }],
+                model: 'gpt-3.5-turbo'
+            }),
+            onload: onLoad,
+            onerror: function(error) { if (getShowAnswer.attemptCnt < 1) {
+            	retryDiffHost() } else { braveGPTConsoleError(error) }}
+        })
+
+        function retryDiffHost() {
+            braveGPTConsoleWarn(`Error calling ${ randomEndpoint }. Trying another endpoint...`)
+            getShowAnswer.triedAPIhosts.push(randomAPIhost) // store current API host to not retry
+            getShowAnswer.attemptCnt++
+            getShowAnswer(question, callback)
+        }
+
+        function onLoad(event) {
+            if (event.status !== 200 && getShowAnswer.attemptCnt < 1) { retryDiffHost() }
+            else if (event.status === 429) { braveGPTdivAlert(braveGPTdivAlerts.tooManyRequests) }
+            else if (event.responseText) {
+                try {
+                    var answer = JSON.parse(event.responseText).choices[0].message.content
+                    braveGPTshow(answer) ; getShowAnswer.triedAPIhosts = [] ; getShowAnswer.attemptCnt = 0
+                } catch (error) { braveGPTConsoleError('Failed to parse response JSON: ' + error) }
+        }}
+    }
+
+    function braveGPTshow(answer) {
+        braveGPTdiv.innerHTML = `${ navigator.userAgent.includes('Firefox') ? // only load robot emoji in FF + forks
+            '<span class="chatgpt-icon"><img width=25 src="https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/bravegpt/media/images/robot-emoji.png"></span>' : '' }<span class="prefix">ChatGPT</span><pre></pre>`
+        braveGPTdiv.querySelector('pre').textContent = answer
+        braveGPTdiv.appendChild(chatGPTfooter) // append feedback link
+    }
+
+    async function main() {
+        braveGPTdiv.innerHTML = '<p class="loading"></p>' // give BraveGPT container spinning loader
+        var siderbarContainer = document.querySelector('#side-right')
+        siderbarContainer.prepend(braveGPTdiv) // inject BraveGPT container
+        getShowAnswer(new URL(window.location.href).searchParams.get('q')) // get/show answer
+    }
+
+})()
